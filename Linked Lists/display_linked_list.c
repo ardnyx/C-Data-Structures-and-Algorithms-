c57fbf6b9eb5673df01fe9ba9459d8ed210a7bcc
reiyane <dannleyesa@gmail.com>
2026-05-15 23:10:54 +0800

    added sum and count function

--- Linked Lists/display_linked_list.c
+++ Linked Lists/display_linked_list.c
@@ -4,15 +4,15 @@
 struct Node {
   int data; // This creates the place to put values
   struct Node *next; // This creates the place to address pointing to next nodes
-} *first = NULL; // *first is a pointer to a Node, currently pointing nowehere
+} *first_node = NULL; // *first_node is a pointer to a Node, currently pointing nowehere
 
 void CreateLinkedList(int A[], int n) {
     int i;
     struct Node *t, *last; // *t is a helper pointer, *last is pointing to last node
-    first = (struct Node *)malloc(sizeof(struct Node)); 
-    first->data = A[0];
-    first->next = NULL;
-    last=first;
+    first_node = (struct Node *)malloc(sizeof(struct Node)); 
+    first_node->data = A[0];
+    first_node->next = NULL;
+    last=first_node;
 
     for (i = 1; i < n; i++) {
         t = (struct Node *)malloc(sizeof(struct Node));
@@ -23,44 +23,81 @@ void CreateLinkedList(int A[], int n) {
     }
 }
 
-void RecursiveDisplay(struct Node *p) {
-    if (p != NULL) {
-        printf("%d", p->data);
-        if (p->next != NULL) {
+void RecursiveDisplay(struct Node *node) {
+    if (node != NULL) {
+        printf("%d", node->data);
+        if (node->next != NULL) {
             printf(" -> ");
         }
-        RecursiveDisplay(p->next);
+        RecursiveDisplay(node->next);
     }
 }
 
-void Reverse_RecursiveDisplay(struct Node *p) {
-    if (p != NULL) {
-        Reverse_RecursiveDisplay(p->next);
-        if (p->next != NULL) {
+void Reverse_RecursiveDisplay(struct Node *node) {
+    if (node != NULL) {
+        Reverse_RecursiveDisplay(node->next);
+        if (node->next != NULL) {
             printf(" -> ");
         }
-        printf("%d", p->data);
+        printf("%d", node->data);
     }
 }
 
 void NormalDisplay() {
-    struct Node *p = first;
-    while (p != NULL) {
-        printf("%d", p->data);
-        if (p->next != NULL) {
+    struct Node *node = first_node;
+    while (node != NULL) {
+        printf("%d", node->data);
+        if (node->next != NULL) {
             printf(" -> ");
         }
-        p = p->next;
+        node = node->next;
     }
     return;
 }
 
+int Count(struct Node *node) {
+    int count = 0;
+    while (node != NULL) {
+        count++;
+        node = node->next;
+    }
+    return count;
+}
+
+int RecursiveCount(struct Node *node) {
+    if (node == 0){
+        return 0;
+    }
+    else {
+        return RecursiveCount(node->next) + 1; // 1 + RecursiveCount(node->next) does not 
+                                               // work because what would happen is 
+                                               // 1 + 0x<memory> (invalid)
+    }
+}
+
+int Sum(struct Node *node) {    
+    int result = 0;
+    while (node != NULL) {
+        result += node->data;
+        node = node->next;
+    }
+    return result;
+}
+
+int RecursiveSum(struct Node *node) {
+    if (node != NULL) {
+        return RecursiveSum(node->next) + node->data;
+    }
+    else {
+        return 0;
+    }
+}
 int main() {
     int A[] = {1,2,3,4,5};
     CreateLinkedList(A, sizeof(A) / sizeof(int));
 
     printf("Recursive Display: ");
-    RecursiveDisplay(first);
+    RecursiveDisplay(first_node);
     printf("\n");
 
     printf("Normal Display: ");
@@ -68,6 +105,12 @@ int main() {
     printf("\n");
 
     printf("Reversed Recursive Display: ");
-    Reverse_RecursiveDisplay(first);
+    Reverse_RecursiveDisplay(first_node);
+
+    int count_result = Count(first_node);
+    int sum_result = Sum(first_node);  
+
+    int r_count_result = RecursiveCount(first_node);
+    int r_sum_result = RecursiveSum(first_node);
     return 0;
 }
